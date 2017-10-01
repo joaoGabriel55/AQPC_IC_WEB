e91b20b3eb241b888f051e6a45cb80f812c1a160
Quaresma <j.quaresmasantos_98@hotmail.com>
2017-10-01 12:25:16 -0300

    Garibada na interface

--- AQPC_WEB/src/Principal2.java
+++ AQPC_WEB/src/Principal2.java
@@ -30,10 +30,17 @@ import javax.swing.JSeparator;
 import javax.swing.JCheckBox;
 import java.awt.Color;
 import java.awt.Label;
+import java.awt.Canvas;
+import javax.swing.JComboBox;
+import javax.swing.ButtonGroup;
+import javax.swing.ButtonModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.persistence.EnumType;
+import javax.swing.JRadioButton;
 
 public class Principal2 {
 
-	private JFrame frame;
+	private JFrame frmAqpc;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
@@ -49,7 +56,7 @@ public class Principal2 {
 			public void run() {
 				try {
 					Principal2 window = new Principal2();
-					window.frame.setVisible(true);
+					window.frmAqpc.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -75,9 +82,10 @@ public class Principal2 {
 			System.err.println("Can't load file");
 			System.exit(1);
 		}
-		frame = new JFrame();
-		frame.setBounds(100, 100, 755, 448);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAqpc = new JFrame();
+		frmAqpc.setTitle("AQPC");
+		frmAqpc.setBounds(100, 100, 755, 535);
+		frmAqpc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JLabel lblNewLabel = new JLabel("Qual a quantidade de cores? ");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -115,47 +123,8 @@ public class Principal2 {
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
 
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-
-		JLabel lblNewLabel_6 = new JLabel();
-		lblNewLabel_6.setIcon(new ImageIcon("C:\\Users\\fbio_\\Pictures\\help.png"));
-		lblNewLabel_6.addMouseListener(new MouseListener() {
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				JOptionPane.showMessageDialog(lblNewLabel_6, "Os tipos de sobremesa que existem:\n1- DOCE\n2 - FRUTA");
-			}
-		});
-		;
-
 		JLabel lblNewLabel_7 = new JLabel();
-		lblNewLabel_7.setIcon(new ImageIcon("C:\\Users\\fbio_\\Pictures\\help.png"));
+		lblNewLabel_7.setIcon(new ImageIcon("src\\img\\infoTab.png"));
 		lblNewLabel_7.addMouseListener(new MouseListener() {
 
 			@Override
@@ -189,118 +158,188 @@ public class Principal2 {
 						"Os tipos de técnicas de cocção:\n1-CONDUCAO\n2-CONVECCAO\n3-GRELHAR\n4-ASSAR\n5-GUISAR\n6-BRASEAR\n7-SALTEAR\n8-CALOR_SECO\n9-CALOR_UMIDO\n10-FRITAR\n11-REFOGAR\n12-RADIACAO");
 			}
 		});
+		
+		
+		
+		
 
-		JLabel lblNewLabel_8 = new JLabel("Avaliação qualitativa das preparações do cardápio");
-		lblNewLabel_8.setForeground(new Color(0, 0, 128));
-		lblNewLabel_8.setFont(new Font("Trebuchet MS", Font.BOLD, 24));
-		
-		JSeparator separator = new JSeparator();
-		
-		JLabel lblVerGrficos = new JLabel("Ver gr\u00E1ficos?");
+		JLabel lblVerGrficos = new JLabel("Ver todos os gr\u00E1ficos?");
 		lblVerGrficos.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		
+
 		JCheckBox checkBox = new JCheckBox("");
-		
+
 		JSeparator separator_1 = new JSeparator();
 
-		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		JLabel lblNewLabel_8 = new JLabel("");
+		lblNewLabel_8.setIcon(new ImageIcon(Principal2.class.getResource("/img/menu_logo.png")));
+
+		JSeparator separator_2 = new JSeparator();
+
+		ButtonGroup grupo = new ButtonGroup();
+
+		JLabel lblVerGrficoFinal = new JLabel("Ver gr\u00E1fico final?");
+		lblVerGrficoFinal.setFont(new Font("Tahoma", Font.PLAIN, 14));
+
+		JCheckBox checkBox_1 = new JCheckBox("");
+
+		JLabel lblNewLabel_6 = new JLabel("New label");
+		lblNewLabel_6.setIcon(new ImageIcon(Principal2.class.getResource("/img/ufrn.png")));
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		
+		JLabel label = new JLabel();
+		label.setIcon(new ImageIcon("src\\img\\infoTab.png"));
+		label.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				JOptionPane.showMessageDialog(lblNewLabel_7,
+						"Tipos de sobremesa:\n1-DOCE\n2-FRUTA");
+			}
+		});
+
+		GroupLayout groupLayout = new GroupLayout(frmAqpc.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(74)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(btnNewButton)
-							.addContainerGap())
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+							.addGap(71)
+							.addComponent(lblNewLabel_8))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(72)
+							.addComponent(btnNewButton))
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
 							.addGroup(groupLayout.createSequentialGroup()
-								.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-									.addComponent(separator_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
-									.addComponent(separator, GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
-									.addComponent(lblNewLabel_8, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 598, GroupLayout.PREFERRED_SIZE))
-								.addContainerGap(35, Short.MAX_VALUE))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(lblVerGrficos)
-								.addGap(18)
-								.addComponent(checkBox)
-								.addContainerGap())
-							.addGroup(groupLayout.createSequentialGroup()
+								.addGap(67)
 								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 									.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(lblNewLabel_5)
-										.addPreferredGap(ComponentPlacement.UNRELATED)
-										.addComponent(lblNewLabel_6))
-									.addGroup(groupLayout.createSequentialGroup()
-										.addPreferredGap(ComponentPlacement.RELATED)
 										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-											.addGroup(groupLayout.createSequentialGroup()
-												.addComponent(lblNewLabel_3)
-												.addPreferredGap(ComponentPlacement.UNRELATED)
-												.addComponent(lblNewLabel_7))
-											.addComponent(lblNewLabel_4)
 											.addComponent(lblNewLabel_1)
 											.addComponent(lblNewLabel_2)
-											.addComponent(lblNewLabel))))
-								.addGap(36)
+											.addComponent(lblNewLabel)
+											.addComponent(lblNewLabel_4)
+											.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+												.addGroup(groupLayout.createSequentialGroup()
+													.addComponent(lblNewLabel_3)
+													.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+													.addComponent(lblNewLabel_7, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE))
+												.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+													.addComponent(lblNewLabel_5)
+													.addGap(18)
+													.addComponent(label, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE))))
+										.addPreferredGap(ComponentPlacement.RELATED, 278, Short.MAX_VALUE)
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+											.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, 59, GroupLayout.PREFERRED_SIZE)
+											.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+												.addComponent(textField_4, 0, 0, Short.MAX_VALUE)
+												.addComponent(textField_3, 0, 0, Short.MAX_VALUE)
+												.addComponent(textField_2, 0, 0, Short.MAX_VALUE)
+												.addComponent(textField_1, 0, 0, Short.MAX_VALUE)
+												.addComponent(textField, GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE))))
+									.addGroup(groupLayout.createSequentialGroup()
+										.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+											.addComponent(lblVerGrficoFinal, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+											.addComponent(lblVerGrficos))
+										.addPreferredGap(ComponentPlacement.UNRELATED)
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+											.addComponent(checkBox_1, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+											.addComponent(checkBox)))))
+							.addGroup(groupLayout.createSequentialGroup()
+								.addGap(52)
 								.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-									.addComponent(textField, GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)
-									.addComponent(textField_1, Alignment.LEADING, 0, 0, Short.MAX_VALUE)
-									.addComponent(textField_5, Alignment.LEADING, 0, 0, Short.MAX_VALUE)
-									.addComponent(textField_4, Alignment.LEADING, 0, 0, Short.MAX_VALUE)
-									.addComponent(textField_3, Alignment.LEADING, 0, 0, Short.MAX_VALUE)
-									.addComponent(textField_2, Alignment.LEADING, 0, 0, Short.MAX_VALUE))
-								.addGap(293)))))
+									.addComponent(lblNewLabel_6, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)
+									.addComponent(separator_2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 639, Short.MAX_VALUE)
+									.addComponent(separator_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 639, Short.MAX_VALUE)))))
+					.addContainerGap(48, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(37)
+					.addGap(22)
 					.addComponent(lblNewLabel_8)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(separator_1, GroupLayout.PREFERRED_SIZE, 4, GroupLayout.PREFERRED_SIZE)
-					.addGap(32)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lblNewLabel)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_1))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_2))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_4))
-					.addGap(11)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lblNewLabel_3)
-						.addComponent(lblNewLabel_7)
-						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addComponent(separator_1, GroupLayout.PREFERRED_SIZE, 11, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-							.addComponent(lblNewLabel_5)
-							.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addComponent(lblNewLabel_6))
-					.addGap(12)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(checkBox, 0, 0, Short.MAX_VALUE)
-						.addComponent(lblVerGrficos, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addGap(13)
-					.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblNewLabel)
+							.addGap(18)
+							.addComponent(lblNewLabel_1)
+							.addGap(18)
+							.addComponent(lblNewLabel_2)
+							.addGap(18)
+							.addComponent(lblNewLabel_4)
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel_3)
+								.addComponent(lblNewLabel_7, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblNewLabel_5)
+									.addGap(13)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(checkBox)
+										.addComponent(lblVerGrficos))
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(checkBox_1, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblVerGrficoFinal, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)))
+								.addComponent(label, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addGap(24)
+					.addComponent(separator_2, GroupLayout.PREFERRED_SIZE, 4, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btnNewButton)
-					.addGap(20))
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton)
+						.addComponent(lblNewLabel_6, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
 		);
-		frame.getContentPane().setLayout(groupLayout);
+		frmAqpc.getContentPane().setLayout(groupLayout);
 
 		btnNewButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// JFuzzyChart.get().chart(fis.getFunctionBlock("aqpc"));
+				if (checkBox_1.isSelected())
+					JFuzzyChart.get().chart(fis.getFunctionBlock("aqpc"));
 
 				String cor = textField.getText();
 				String folhosos = textField_2.getText();
@@ -308,7 +347,7 @@ public class Principal2 {
 				String enxofre = textField_1.getText();
 				String gordura = textField_3.getText();
 				String sobremesa = textField_5.getText();
-
+				
 				fis.setVariable("cor", Double.valueOf(cor));
 				fis.setVariable("folhosos", Double.valueOf(folhosos));
 				fis.setVariable("tecnicasCoccao", Double.valueOf(tecnicasCoccao));
@@ -316,13 +355,20 @@ public class Principal2 {
 				fis.setVariable("gordura", Double.valueOf(gordura));
 				fis.setVariable("sobremesa", Double.valueOf(sobremesa));
 
+				/*if (doce.isSelected() == true)
+					fis.setVariable("sobremesa", 1);
+
+				if (fruta.isSelected() == true)
+					fis.setVariable("sobremesa", 2);
+*/
 				fis.evaluate();
 
 				Variable aqpc = fis.getFunctionBlock("aqpc").getVariable("avaliacao");
-				
-				if(checkBox.isSelected())
+
+				if (checkBox.isSelected())
 					JFuzzyChart.get().chart(aqpc, aqpc.getDefuzzifier(), true);
-				frame.setVisible(false);
+
+				frmAqpc.setVisible(false);
 
 				JLabel teste = new JLabel();
 				DecimalFormat decimalFormat = new DecimalFormat("#.##");
